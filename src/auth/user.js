1c93dd1a17993926f801813e69dfb52b6c402bad
reactionic127 <richard.gill127@gmail.com>
2017-10-28 10:00:33 +0200

    remove product info is removed

--- src/auth/user.js
+++ src/auth/user.js
@@ -1,9 +1,10 @@
 export default {
     get isAuthenticated () {
-        return window.localStorage.getItem('isAuthenticated')
+        return window.localStorage.getItem('isAuthenticated');
     },
     logout () {
-        window.localStorage.removeItem('isAuthenticated')
+        window.localStorage.removeItem('isAuthenticated');
+        window.localStorage.removeItem('inventoryInfo');
     },
     setUser(user) {
         window.localStorage.setItem('authUser', JSON.stringify(user))
