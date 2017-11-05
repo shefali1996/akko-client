c01604dbb2c3375f78bf114414e14af810e7301d
reactionic127 <richard.gill127@gmail.com>
2017-11-05 15:34:46 +0100

    added comment view

--- src/containers/SetCogs.js
+++ src/containers/SetCogs.js
@@ -53,82 +53,98 @@ class SetCogs extends Component {
                             Account Setup
                         </span>
                     </Row>
-                    <div className="text-center margin-t-40">
-                        <span className="select-style-text">
-                            Set COGS for your products
-                        </span>
-                    </div>
-                    <div className="text-center margin-t-5">
-                        <span className="select-style-comment">
-                            We will use these Cost of Goods Sold (COGS) estimates to calculate your gross profit
-                        </span>
-                    </div>
-                    <div className="text-center margin-t-5">
-                        <span className="select-style-comment-small">
-                            ( you can update these anytime from the Settings menu )
-                        </span>
-                    </div>
-                    <div className="flex-center margin-t-40">
-                        <span className="select-style-comment">
-                            We found 112 products in 364 variants from your shop. How do you
-                        </span>
-                    </div>
-                    <div className="flex-center margin-t-5">
-                        <span className="select-style-comment">
-                            want to set COGS for all these products?
-                        </span>
-                    </div>
-                    <div className="flex-center margin-t-40">
-                        <div className="style-container flex-center">
-                            <div className="style-icon-view">
-                                <Image src={cogs1} className="business-icon" />
-                            </div>
-                            <div className="style-text-view">
-                                <span className="select-text-large">
-                                    ENTER MANUALLY
-                                </span>
-                                <span className="select-text-small">
-                                    Enter COGS for each product manually.
+                    <Row>
+                        <Col md={6} mdOffset={3}>
+                            <div className="text-center margin-t-40">
+                                <span className="select-style-text">
+                                    Set COGS for your products
                                 </span>
                             </div>
-                        </div>
-                    </div>
-                    <div className="flex-center margin-t-10">
-                        <div className="style-container flex-center">
-                            <div className="style-icon-view">
-                                <Image src={cogs2} className="business-icon" />
-                            </div>
-                            <div className="style-text-view">
-                                <span className="select-text-large">
-                                    UPLOAD CSV
-                                </span>
-                                <span className="select-text-small">
-                                    We will prefill the SKUs in a CSV which you can download, enter
-                                </span>
-                                <span className="select-text-small">
-                                    COGS and upload.
+                            <div className="text-center margin-t-5">
+                                <span className="select-style-comment">
+                                    We will use these Cost of Goods Sold (COGS) estimates to calculate your gross profit
                                 </span>
                             </div>
-                        </div>
-                    </div>
-                    <div className="flex-center margin-t-10">
-                        <div className="style-container flex-center">
-                            <div className="style-icon-view">
-                                <Image src={cogs3} className="business-icon" />
-                            </div>
-                            <div className="style-text-view">
-                                <span className="select-text-large">
-                                    SET MARKUP
-                                </span>
-                                <span className="select-text-small">
-                                    You can set the markup that you charge and we can back
-                                </span>
-                                <span className="select-text-small">
-                                    calculate your COGS by comparing it against your selling price.
+                            <div className="text-center margin-t-5">
+                                <span className="select-style-comment-small">
+                                    ( you can update these anytime from the Settings menu )
                                 </span>
                             </div>
-                        </div>
-                    </div>
+                            <div className="flex-center margin-t-40">
+                                <span className="select-style-comment">
+                                    We found 112 products in 364 variants from your shop. How do you
+                                </span>
+                            </div>
+                            <div className="flex-center margin-t-5">
+                                <span className="select-style-comment">
+                                    want to set COGS for all these products?
+                                </span>
+                            </div>
+                            <div className="flex-center margin-t-40">
+                                <div className="style-container flex-center">
+                                    <div className="style-icon-view">
+                                        <Image src={cogs1} className="business-icon" />
+                                    </div>
+                                    <div className="style-text-view">
+                                        <span className="select-text-large">
+                                            ENTER MANUALLY
+                                        </span>
+                                        <span className="select-text-small">
+                                            Enter COGS for each product manually.
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex-center margin-t-10">
+                                <div className="style-container flex-center">
+                                    <div className="style-icon-view">
+                                        <Image src={cogs2} className="business-icon" />
+                                    </div>
+                                    <div className="style-text-view">
+                                        <span className="select-text-large">
+                                            UPLOAD CSV
+                                        </span>
+                                        <span className="select-text-small">
+                                            We will prefill the SKUs in a CSV which you can download, enter
+                                        </span>
+                                        <span className="select-text-small">
+                                            COGS and upload.
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex-center margin-t-10">
+                                <div className="style-container flex-center">
+                                    <div className="style-icon-view">
+                                        <Image src={cogs3} className="business-icon" />
+                                    </div>
+                                    <div className="style-text-view">
+                                        <span className="select-text-large">
+                                            SET MARKUP
+                                        </span>
+                                        <span className="select-text-small">
+                                            You can set the markup that you charge and we can back
+                                        </span>
+                                        <span className="select-text-small">
+                                            calculate your COGS by comparing it against your selling price.
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col md={3}>
+                            <div className="description-view margin-t-40 text-center">
+                                <span className="select-style-comment">
+                                    COGS is the cost of buying one unit of the product from your vendor.
+                                </span>
+                            </div>
+                            <div className="description-view margin-t-10 text-center">
+                                <span className="select-style-comment">
+                                    Do not include costs incurred when selling the product, like Shipping, Tax or Discounts.
+                                </span>
+                            </div>
+                        </Col>
+                    </Row>
                     <div className="text-center margin-t-50">
                         <Button className="skip-button" onClick={this.onConnect}>
                             SKIP FOR NOW
