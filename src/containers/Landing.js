b3a84823c20c4451344b8223f2291eb885ae1b3e
reactionic127 <richard.gill127@gmail.com>
2017-11-11 01:20:11 +0200

    responisve implemented

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -32,19 +32,20 @@ class Landing extends Component {
             <Grid className="main-layout">
                 <Header history={this.props.history} />
                 <Row>
-                    <Col md={12} className="text-center padding-t-66">
+                    <Col md={12} className="productive-text-view">
                         <p className="large-title">
                             Predictive business analytics for eCommerce
                         </p>
                     </Col>
-                    <Col md={12}>
+                    <Col md={12} className="flex-center">
                         <p className="middle-title">
-                            Prevent customer churn and stockouts before they happen. Track your profits, sales, 
+                            Prevent customer churn and stockouts before they happen. Track your profits, sales
+                            customer behavior and inventory in realtime, all from a single dashboard.
                         </p>
                     </Col>
                     <Col md={12}>
                         <p className="middle-title">
-                            customer behavior and inventory in realtime, all from a single dashboard.
+                            
                         </p>
                     </Col>
                     <Col md={12} className="inline-block text-center margin-t-20">
@@ -58,9 +59,9 @@ class Landing extends Component {
                             . More integrations coming soon.
                         </span>
                     </Col>
-                    <Col md={12} className="margin-t-60 no-padding">
+                    <Col md={12} className="productive-text-view">
                         <Col md={6} className="no-padding">
-                            <div className="text-center margin-t-40">
+                            <div className="request-view">
                                 <p className="invite-text">
                                     Request an invite to get early access!
                                 </p>
