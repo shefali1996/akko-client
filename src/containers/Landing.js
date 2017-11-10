68ab54aa7e4ff2b3a9b6560d3f59dd32cf42776e
reactionic127 <richard.gill127@gmail.com>
2017-11-10 18:23:26 +0200

    label is removed

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -22,19 +22,19 @@ class Landing extends Component {
                 <Header history={this.props.history} />
                 <Row>
                     <Col md={12} className="text-center padding-t-66">
-                        <Label className="large-title">
+                        <p className="large-title">
                             Predictive business analytics for eCommerce
-                        </Label>
+                        </p>
                     </Col>
                     <Col md={12} className="text-center padding-t-20">
-                        <Label className="middle-title">
+                        <p className="middle-title">
                             Prevent customer churn and stockouts before they happen. Track your profits, sales, 
-                        </Label>
+                        </p>
                     </Col>
                     <Col md={12} className="text-center padding-t-5">
-                        <Label className="middle-title">
+                        <p className="middle-title">
                             customer behavior and inventory in realtime, all from a single dashboard.
-                        </Label>
+                        </p>
                     </Col>
                     <Col md={12} className="padding-t-30">
                                                 
