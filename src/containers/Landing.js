864cf2968f9fe105a8b0092df5309af26cd7bea4
reactionic127 <richard.gill127@gmail.com>
2017-11-10 19:15:06 +0200

    added request view

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Grid, Row, Col, Label } from 'react-bootstrap';
 import Header from '../components/Header';
 import '../styles/App.css';
+import shopifyicon from '../assets/shopifyicon.svg';
 
 class Landing extends Component {
     constructor(props) {
@@ -26,18 +27,43 @@ class Landing extends Component {
                             Predictive business analytics for eCommerce
                         </p>
                     </Col>
-                    <Col md={12} className="text-center padding-t-20">
+                    <Col md={12}>
                         <p className="middle-title">
                             Prevent customer churn and stockouts before they happen. Track your profits, sales, 
                         </p>
                     </Col>
-                    <Col md={12} className="text-center padding-t-5">
+                    <Col md={12}>
                         <p className="middle-title">
                             customer behavior and inventory in realtime, all from a single dashboard.
                         </p>
                     </Col>
-                    <Col md={12} className="padding-t-30">
-                                                
+                    <Col md={12} className="inline-block text-center margin-t-20">
+                        <span className="small-title">
+                            Works with
+                        </span>
+                        <span>
+                            <img src={shopifyicon} className="shopify-icon"/>
+                        </span>
+                        <span className="small-title">
+                            . More integrations coming soon.
+                        </span>
+                    </Col>
+                    <Col md={12} className="margin-t-20">
+                        <Col md={6}>
+                            <div className="text-center">
+                                <p className="invite-text">
+                                    Request an invite to get early access!
+                                </p>
+                            </div>
+                        </Col>
+                        <Col md={6}>
+                            <Col md={7}>
+                                
+                            </Col>
+                            <Col md={5}>
+
+                            </Col>
+                        </Col>
                     </Col>
                 </Row>
             </Grid>
