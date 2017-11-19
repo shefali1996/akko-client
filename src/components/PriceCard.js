8fa0ffc7fb4b2430eb8688c7f8e6bfffa4b41645
reactionic127 <richard.gill127@gmail.com>
2017-11-19 23:35:54 +0200

    removed useless import

--- src/components/PriceCard.js
+++ src/components/PriceCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
 import PriceBox from '../components/PriceBox';
 import statistics from '../assets/statistics.png';
 
