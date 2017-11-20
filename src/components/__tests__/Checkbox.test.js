a33d29751ff58bcfb40a5395296bb21eb58ddb16
reactionic127 <richard.gill127@gmail.com>
2017-11-20 12:09:35 +0200

    test added for all components

---
+++ src/components/__tests__/Checkbox.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import test from 'ava';
+
+test('render header correctly', t => {
+  t.pass();
+});
+
+test.todo('Offset a number by an amount');
---
+++ src/components/__tests__/Navigationbar.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import test from 'ava';
+
+test('render header correctly', t => {
+  t.pass();
+});
