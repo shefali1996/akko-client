f6710ede110e9ba572002b576dc09b59ee1fc57a
reactionic127 <richard.gill127@gmail.com>
2017-11-21 23:45:16 +0200

    useless styles are removed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './styles/index.css';
 import App from './containers/App';
 import 'font-awesome/css/font-awesome.min.css';
-import { BrowserRouter as Router } from "react-router-dom"; // \todo: required?
+import { BrowserRouter as Router } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import 'sweetalert/dist/sweetalert.css';
 
