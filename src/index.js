801e0f45760e080d021f4e3f0f72eff32774559d
reactionic127 <richard.gill127@gmail.com>
2017-11-22 16:40:10 +0200

    eslint is fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import { BrowserRouter as Router } from 'react-router-dom'; // \todo: required?
 import './styles/index.css';
 import App from './containers/App';
-import 'font-awesome/css/font-awesome.min.css';
-import { BrowserRouter as Router } from "react-router-dom"; // \todo: required?
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 registerServiceWorker();
