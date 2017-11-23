d1cf1e4332b2ddf456564e4f254bb8b94b9b6211
reactionic127 <richard.gill127@gmail.com>
2017-11-23 17:18:18 +0200

    build configuration is added

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export default {
   apiGateway: {
-  URL: "https://9iotp620vl.execute-api.us-east-2.amazonaws.com/prod",
-  REGION: "us-east-2"
+    URL: 'https://9iotp620vl.execute-api.us-east-2.amazonaws.com/prod',
+    REGION: 'us-east-2'
   },
   cognito: {
-    USER_POOL_ID: "us-east-2_cnijwbIs9",
-    APP_CLIENT_ID: "2lboppsjmcoe9jsb1ef773afah",
-    REGION: "us-east-2",
-    IDENTITY_POOL_ID: "us-east-2:f33bd5bd-b07f-428a-bfff-59639596bccf"
+    USER_POOL_ID: 'us-east-2_cnijwbIs9',
+    APP_CLIENT_ID: '2lboppsjmcoe9jsb1ef773afah',
+    REGION: 'us-east-2',
+    IDENTITY_POOL_ID: 'us-east-2:f33bd5bd-b07f-428a-bfff-59639596bccf'
   }
 };
