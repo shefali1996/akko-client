f447acc61ca1b5a06b0fefeb2e300c7dd1d0a18e
reactionic127 <richard.gill127@gmail.com>
2017-11-23 10:08:28 +0200

    added stable version

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import 'font-awesome/css/font-awesome.min.css';
 import 'sweetalert/dist/sweetalert.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import './styles/app.scss';
+import './styles/App.css';
+// import './styles/app.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
