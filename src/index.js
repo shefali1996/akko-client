3dc4669dc579d9749949b59a5969cdcc0e122634
reactionic127 <richard.gill127@gmail.com>
2017-11-23 10:39:21 +0200

    router issue is fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import 'font-awesome/css/font-awesome.min.css';
 import 'sweetalert/dist/sweetalert.css';
-import App from 'containers/App';
+import App from './containers/App';
 import './styles/app.scss';
 
 console.log('aaa');
