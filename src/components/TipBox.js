b2177f18f2f0a3476451f02a445baa527d199a63
reactexcel <arun.etech2011@gmail.com>
2017-11-24 13:34:55 +0530

    tip text is added as a new component

--- src/components/TipBox.js
+++ src/components/TipBox.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Col } from 'react-bootstrap';
+import tipIcon from '../assets/images/tip_icon.svg';
 
 class TipBox extends React.Component {
   render() {
     return(
       <div className="description-view margin-t-40 text-center">
-        <span className="select-style-comment">
-          <b><u>Tip:</u></b> COGS is the cost of buying one unit of the product from your vendor.
-        </span>
+      	<Col md={1}>
+      		<img src={tipIcon} className="tip-icon" alt="icon" />
+        </Col>
+        <Col md={10}>
+	         <span className="select-style-comment">
+	          <b><u>Tip:</u></b> COGS is the cost of buying one unit of the product from your vendor.
+	        </span>
+        </Col>
       </div>
     )
   }
