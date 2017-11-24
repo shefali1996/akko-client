362aa6da6e2530121980ab16d8537c30dbad98c5
reactionic127 <richard.gill127@gmail.com>
2017-11-24 22:07:45 +0200

    nav bar responsive added

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -20,13 +20,13 @@ class Navigationbar extends Component {
   render() {
     return (
       <div className="nav-container">
-        <Row className="no-margin white-bg">
-          <Col md={3} className="flex-left">
+        <Row>
+          <Col md={3} sm={6} xs={6} className="flex-left">
             <Label className="app-title">
               akko
             </Label>
           </Col>
-          <Col md={2} mdOffset={7} className="text-right no-padding">
+          <Col md={2} mdOffset={7} sm={6} xs={6} className="text-right no-padding">
             <DropdownButton
               title={
                 <div>
