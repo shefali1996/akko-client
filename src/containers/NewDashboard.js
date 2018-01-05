47a22c290d5647d7b1137542b55bb658cf891184
anudeep2011 <anudeep2011@gmail.com>
2018-01-05 13:48:40 +0530

    Card border color fix for 0% (#72)
    * Card border color fix for 0%
    
    * added dummy loading cards before metric data is loaded
    
    * using antd spin
    
    * typo fix
    
    * center fix

--- src/containers/NewDashboard.js
+++ src/containers/NewDashboard.js
@@ -3,7 +3,7 @@ import { Row, Col, Label, Button, Image, Grid, Tabs, Tab } from 'react-bootstrap
 import {Card, CardHeader, CardText} from 'material-ui/Card';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-import { Select } from 'antd';
+import { Select, Spin } from 'antd';
 import $ from 'jquery';
 import {clone} from 'lodash';
 import Navigationbar from '../components/Navigationbar';
@@ -15,6 +15,7 @@ import Footer from '../components/Footer';
 import { invokeApig } from '../libs/awsLib';
 import styles from '../constants/styles';
 import invalidImg from '../assets/images/FontAwesome472.svg';
+import ReactPlaceholder from 'react-placeholder';
 
 const moment = require('moment');
 
@@ -27,6 +28,7 @@ class NewDashboard extends Component {
     super(props);
     this.state = {
       metricsData: [],
+	  metricDataLoaded: false,
       width: '25%',
       activeMetricsId: 'none',
       activeChartData: false
@@ -50,7 +52,8 @@ class NewDashboard extends Component {
   getMetrics() {
     invokeApig({ path: '/metrics' }).then((results) => {
       this.setState({
-        metricsData: results.metrics
+        metricsData: results.metrics,
+		metricDataLoaded: true,
       });
     })
       .catch(error => {
@@ -153,6 +156,29 @@ class NewDashboard extends Component {
   render() {
     const {metricsData} = this.state;
     const renderCards = [];
+	if (!this.state.metricDataLoaded) {
+		// Add dummy loading cards
+		const number_of_dummy_cards = 3;
+		const CustomSpin = (
+			<div style={{width:"100%", textAlign: "center"}}>
+				<Spin size='large'/>
+			</div>
+		);
+		for (let i=0; i<number_of_dummy_cards; i++) {
+			renderCards.push(<Col style={{width: this.state.width}} className="dashboard-card-container">
+          <Card
+            className={`price-card-style`}
+            style={styles.metricsCardStyle}
+            >
+            <CardText>
+			<div style={{padding: "40%"}}>
+				<ReactPlaceholder ready={this.state.metricDataLoaded} customPlaceholder={CustomSpin}></ReactPlaceholder>
+			</div>
+            </CardText>
+          </Card>
+					</Col>);
+		}
+	}
     metricsData.map((value, index) => {
       let active = '';
       let borderRed = '';
@@ -163,7 +189,7 @@ class NewDashboard extends Component {
       if (`card_${index}` === this.state.activeMetricsId) {
         active = 'active-metrics';
       }
-      if (value.trend !== '+') {
+      if (value.trend === '-') {
         borderRed = 'border-red';
       }
       let invalid = false;
@@ -195,11 +221,11 @@ class NewDashboard extends Component {
           invalid = true;
           value.title = 'Expenses Breakdown';
         }
-        renderCards.push(<Col key={index} id={`card_${index}`} style={{width: this.state.width}} className="dashboard-card-cantainer expenses-breakdown">
+        renderCards.push(<Col key={index} id={`card_${index}`} style={{width: this.state.width}} className="dashboard-card-container expenses-breakdown">
           {invalid ? this.invalidCard(value) : this.expenseCard(expensesData)}
         </Col>);
       } else {
-        renderCards.push(<Col key={index} id={`card_${index}`} style={{width: this.state.width}} className="dashboard-card-cantainer" title={!invalid ? `Click to explore ${value.title} in detail` : null}>
+        renderCards.push(<Col key={index} id={`card_${index}`} style={{width: this.state.width}} className="dashboard-card-container" title={!invalid ? `Click to explore ${value.title} in detail` : null}>
           {invalid ? this.invalidCard(value) : <Card
             className={`price-card-style ${active} ${borderRed}`}
             onClick={(e) => this.handleClickMetrics(`card_${index}`, value, chartData)}
