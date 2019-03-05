4d4db9be7efc9f898e2e878ed0cc21ef245c3e4c
reactexcel <arun.etech2011@gmail.com>
2019-03-05 13:30:41 +0530

    Error msg resolved in setcogs page (#226)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,11 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
-import immutableCheckMiddleWare from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import reducers from './reducer';
 
 const middleWare = [];
 
 middleWare.push(thunk);
-
-// Immutability Check
-if (process.env.NODE_ENV === 'development') {
-  middleWare.push(immutableCheckMiddleWare());
-}
 
 const loggerMiddleware = createLogger({
   predicate: () => process.env.NODE_ENV === 'development',
